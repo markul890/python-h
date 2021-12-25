62c669078af05fc439b881be1abe35d62062477c
Margo <maasau@gmail.com>
2021-12-25 13:23:49 +0200

    homework

---
+++ н2.py
@@ -0,0 +1,9 @@
+name = input("введите имя: ")
+surname = input ("введите фамилию: ")
+birth_y = input ("введите год рождения: ")
+city = input ("введите город: ")
+email = input ("введите имейл")
+tel = input ("введите телефон")
+
+
+print(f'мое имя {name}  моя фамилия {surname} , год рождения {birth_y}, город проживания {city}, телефон {tel}'  )
---
+++ н3.py
@@ -0,0 +1,3 @@
+def my_f(a,b,c):
+    return max(a, b, c)
+print (my_f(int(input("a")), int(input("b")), int(input("c") )))
