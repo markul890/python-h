8291777fa6e35898c4f36c234709ccac23f3ef2f
Margo <maasau@gmail.com>
2022-01-03 19:51:25 +0200

    home

--- н3.py
+++ н3.py
@@ -1,1 +1,11 @@
-with open ("text_3hw.txt", "r") as f:
+with open ("text_3hw.txt") as file:
+    my_list = [my_line.split() for my_line in file.readlines()]
+
+my_workers = [{"name": a[0], "money": int( a[1])}
+     for a in my_list
+     if len (a) > 1]
+
+for a in my_workers:
+    if a ["money"] < 20000:
+        print(a["name"])
+
